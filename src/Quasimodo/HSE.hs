828b88d2305b22b78eeeabc620f25bdba4215358
Michael Sloan <mgsloan@gmail.com>
2013-09-17 08:16:41 -0700

    Add "convenience" support back in

--- src/Quasimodo/HSE.hs
+++ src/Quasimodo/HSE.hs
@@ -2,13 +2,18 @@
 {-# LANGUAGE TupleSections #-}
 {-# LANGUAGE StandaloneDeriving #-}
 {-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeSynonymInstances #-}
+{-# LANGUAGE FlexibleInstances #-}
 {-# OPTIONS_GHC -fno-warn-orphans #-}
-module Quasimodo.HSE where
+module Quasimodo.HSE
+  ( e', ec, p', pc, t', tc, d', dc
+  , toExp, toPat, toType
+  ) where
 
 import Control.Applicative        ( (<$>) )
 import Control.Monad.Trans.Class  ( lift )
 import Control.Monad.Trans.Either ( EitherT(..), hoistEither )
-import Data.Char                  ( isSpace )
+import Data.Char                  ( isSpace, isUpper )
 import Data.Either                ( rights )
 import Text.Themplates            ( Chunk, substSplices, parseSplices
                                   , curlySplice, generateNames )
@@ -25,19 +30,32 @@ import qualified Language.Haskell.Meta     as Meta
    antiquotes.
  -}
 
-e', p', t', d' :: TH.QuasiQuoter
-e' = astQuoter "Exp"  parseExp'
-p' = astQuoter "Pat"  parsePat'
-t' = astQuoter "Type" parseType'
-d' = astQuoter "Decs" parseDecs'
+e', ec, p', pc, t', tc, d', dc :: TH.QuasiQuoter
+e' = astQuoter False "Exp"  parseExp'
+ec = astQuoter True  "Exp"  parseExp'
+
+p' = astQuoter False "Pat"  parsePat'
+pc = astQuoter True  "Pat"  parsePat'
+
+t' = astQuoter False "Type" parseType'
+tc = astQuoter True  "Type" parseType'
+
+d' = astQuoter False "Decs" parseDecs'
+dc = astQuoter True  "Decs" parseDecs'
+
+class ToExp      a where toExp      :: a -> Exts.Exp
+class ToPat      a where toPat      :: a -> Exts.Pat
+class ToType     a where toType     :: a -> Exts.Type
+class ToDecl     a where toDecl     :: a -> Exts.Decl
 
 -- | Builds a quoter for an AST, given a few configurations.  This is the
 --   function that is used to implement @e'@, @p'@, @t'@, and @d'@.
 astQuoter :: forall a. TH.Lift a
-          => String
+          => Bool
+          -> String
           -> (String -> Either String a)
           -> TH.QuasiQuoter
-astQuoter name parser = TH.QuasiQuoter expr undefined undefined undefined
+astQuoter use_conv name parser = TH.QuasiQuoter expr undefined undefined undefined
  where
   errEitherT e = either fail return =<< runEitherT e
 
@@ -48,29 +66,24 @@ astQuoter name parser = TH.QuasiQuoter expr undefined undefined undefined
   parse_ast lifter p = lift . lifter =<< p
 
   parse_splice :: (forall b. TH.Lift b => b -> TH.Q c)
---               -> (TH.Exp -> c -> c)
+               -> (TH.Name -> c -> c)
                -> (String -> ErrorQ c)
-               -> (String, String) -> ErrorQ [(c, c)]
-  parse_splice lifter {- conv -} p (pun, code) = do
+               -> (String, String)
+               -> ErrorQ [(c, c)]
+  parse_splice lifter conv p (pun, code) = do
     parsed <- p code
     case results' of
       [] -> hoistEither
           $ Left $  "Parse error in fancy splice's pun expression: "
                  ++ pun ++ "\nHere are the different parse errors:\n"
                  ++ unlines (map (\(Left e) -> e) results)
-      _ -> lift $ mapM (fmap (, parsed)) results'
-      {-
-      _  -> lift $ sequence
-        [ (\r' -> (conv (conv_var v) r', parsed)) <$> r
-        | (v, r) <- results']
-      -}
+      _ -> lift $ mapM (fmap $ \(cvar, pun') -> (pun', conv cvar parsed)) results'
    where
     results' = rights results
-    results = [ {- ("toExp" ,) . -} lifter <$> parseExp'  pun
-              , {- ("toPat" ,) . -} lifter <$> parsePat'  pun
-              , {- ("toType",) . -} lifter <$> parseType' pun
+    results = [ fmap ('toExp, )  . lifter <$> parseExp'  pun
+              , fmap ('toPat, )  . lifter <$> parsePat'  pun
+              , fmap ('toType, ) . lifter <$> parseType' pun
               ]
---    conv_var = TH.VarE . TH.mkName . ("Language.Haskell.TH.Convenience." ++)
 
   expr :: String -> TH.ExpQ
   expr input = errEitherT $ do
@@ -83,7 +96,7 @@ astQuoter name parser = TH.QuasiQuoter expr undefined undefined undefined
     ast_p    = parse_ast TH.lift
              . parse_err ("Parse error in pun-substituted " ++ name ++ " AST literal")
              . parser
-    splice_p = parse_splice TH.lift -- (\v e -> if use_conv then TH.AppE v e else e)
+    splice_p = parse_splice TH.lift (\v e -> if use_conv then TH.AppE (TH.VarE v) e else e)
              ( parse_err ("Parse error in " ++ name ++ " AST literal splice")
              . parseExp
              )
@@ -133,6 +146,7 @@ parseAst input = do
     | otherwise = s
   processEmpty x = x
 
+--------------------------------------------------------------------------------
 -- Utils
 
 type ErrorQ = EitherT String TH.Q
@@ -186,7 +200,7 @@ mapLeft  f = mapBoth f id
 mapRight :: (b -> d) -> Either a b -> Either a d
 mapRight f = mapBoth id f
 
-
+--------------------------------------------------------------------------------
 -- Orphans
 --TODO: make a package for them
 
@@ -261,3 +275,86 @@ $(TH.deriveLiftMany
 -- , ''Exts.DeclHead
 -- , ''Exts.InstHead
 -- , ''Exts.FieldDecl
+
+--------------------------------------------------------------------------------
+-- Convenience conversion instances
+-- Note that the commented out stuff refers to TH types because this code comes
+-- from quasi-extras, and used to be used for TH AST quoting.
+
+{-
+class ToBody     a where toBody     :: a -> Body
+class ToStmt     a where toStmt     :: a -> Stmt
+class ToMatch    a where toMatch    :: a -> Match
+class ToGuard    a where toGuard    :: a -> Guard
+class ToTyVarBndr a where toTyVarBndr :: a -> TyVarBndr
+-}
+
+instance ToExp   Exts.Exp   where toExp   = id
+instance ToPat   Exts.Pat   where toPat   = id
+instance ToType  Exts.Type  where toType  = id
+instance ToDecl  Exts.Decl  where toDecl  = id
+{-
+instance ToBody  Body  where toBody  = id
+instance ToStmt  Stmt  where toStmt  = id
+instance ToMatch Match where toMatch = id
+instance ToGuard Guard where toGuard = id
+-}
+
+isConName :: Exts.Name -> Bool
+isConName (Exts.Ident n) = isUpper $ head n
+isConName (Exts.Symbol (':':_)) = True
+isConName _ = False
+
+ifCon :: (Exts.QName -> a) -> (Exts.Name -> a) -> Exts.QName -> a
+ifCon f g qn@(Exts.Qual _ n) = if isConName n then f qn else g n
+ifCon f g qn@(Exts.UnQual n) = if isConName n then f qn else g n
+ifCon f _ qn@(Exts.Special _) = f qn
+
+ifConQ :: (Exts.QName -> a) -> (Exts.QName -> a) -> Exts.QName -> a
+ifConQ f g qn@(Exts.Qual _ n) = if isConName n then f qn else g qn
+ifConQ f g qn@(Exts.UnQual n) = if isConName n then f qn else g qn
+ifConQ f _ qn@(Exts.Special _) = f qn
+
+--NOTE: this ToPat sucks for constructors..
+
+instance ToExp       Exts.QName where toExp       = ifConQ Exts.Con Exts.Var
+instance ToPat       Exts.QName where toPat       = ifCon (`Exts.PApp` []) Exts.PVar
+instance ToType      Exts.QName where toType      = ifCon Exts.TyCon Exts.TyVar
+{-
+instance ToBody      Exts.QName where toBody      = toBody . toExp
+instance ToStmt      Exts.QName where toStmt      = toStmt . toExp
+instance ToTyVarBndr Exts.QName where toTyVarBndr = PlainTV
+-}
+
+instance ToExp       Exts.Name where toExp       = toExp  . Exts.UnQual
+instance ToPat       Exts.Name where toPat       = toPat  . Exts.UnQual
+instance ToType      Exts.Name where toType      = toType . Exts.UnQual
+
+instance ToExp       String where toExp       = toExp       . Exts.name
+instance ToPat       String where toPat       = toPat       . Exts.name
+instance ToType      String where toType      = toType      . Exts.name
+{-
+instance ToBody      String where toBody      = toBody      . Exts.name
+instance ToStmt      String where toStmt      = toStmt      . Exts.name
+instance ToTyVarBndr String where toTyVarBndr = toTyVarBndr . Exts.name
+-}
+
+{-
+instance ToBody Exp            where toBody = NormalB
+--TODO: good idea?
+instance ToBody [(Guard, Exp)] where toBody = GuardedB
+
+instance ToGuard  Exp   where toGuard = NormalG
+instance ToGuard  Stmt  where toGuard = PatG . (:[])
+instance ToGuard [Stmt] where toGuard = PatG
+
+instance ToStmt Exp where toStmt = NoBindS
+
+--TODO: good idea?
+instance ToTyVarBndr (Name, Kind) where toTyVarBndr = uncurry KindedTV
+-- instance ToPred (Name, [Type])
+-- instance ToPred (Type, Type)
+
+instance (ToPat p, ToBody b, ToDec d) => ToMatch (p, b, [d]) where toMatch (p, b, ds) = Match (toPat p) (toBody b) (map toDec ds)
+instance (ToPat p, ToBody b)          => ToMatch (p, b     ) where toMatch (p, b    ) = Match (toPat p) (toBody b)              []
+-}
